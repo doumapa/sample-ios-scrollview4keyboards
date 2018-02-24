8144ee082426f76f23092bf24242ce0c32e0dc85
makoto kaneko <makoto.berry.kaneko@gmail.com>
2018-02-24 19:47:29 +0900

    Initial Commit

---
+++ KeyboardPos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardPos
+//
+//  Created by makoto kaneko on 2018/02/24.
+//  Copyright © 2018年 makoto kaneko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
